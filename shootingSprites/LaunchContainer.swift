5e578b0349734d0d0d8bbc92b5841b3f3c7e09d2
Chris <4f428288@opayq.com>
2016-03-02 11:52:53 -0800

    Updated scene creation

--- shootingSprites/LaunchContainer.swift
+++ shootingSprites/LaunchContainer.swift
@@ -7,29 +7,52 @@
 //
 
 import UIKit
+import SpriteKit
+
 
 class LaunchContainer: UIViewController {
+
+    @IBOutlet weak var spriteView: SKView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let scene = GameScene(size: spriteView.bounds.size) as GameScene? {
+            // Configure the view.
+            spriteView.showsFPS = true
+            spriteView.showsNodeCount = true
+            spriteView.showsDrawCount = true
+
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            spriteView.ignoresSiblingOrder = true
+
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+
+            spriteView.presentScene(scene)
+        }
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Release any cached data, images, etc that aren't in use.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
-    */
 
 }
